86f54f84520a8e9593ea583d0eb0cb1afda5b204
jaehyungz <blue259@hanmail.net>
2022-03-01 19:51:40 +0900

    네모

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { createRef, useEffect, useRef, useState } from "react";
+import logo from "./logo.svg";
+import styled from "styled-components";
+
+const Canvas = styled.div`
+  width: 100%;
+  height: 500px;
+  border: 1px solid;
+  position: relative;
+`;
+const Box = styled.div<{ x: number; y: number; width: number; height: number }>`
+  width: ${({ width }) => `${width}px`};
+  height: ${({ height }) => `${height}px`};
+  position: absolute;
+  top: ${({ y }) => `${y}px`};
+  left: ${({ x }) => `${x}px`};
+  border: 1px solid red;
+`;
 
 function App() {
+  const [first, setFirst] = useState({
+    x: 0,
+    y: 0,
+  });
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [draw, setDraw] = useState(true);
+
+  const handleDraw = () => {
+    setDraw((prev) => !prev);
+  };
+
+  const hanldeMouseDown = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const { offsetX, offsetY } = e.nativeEvent;
+
+    if (draw && drawStatus) {
+      setFirst({ x: offsetX, y: offsetY });
+    }
+  };
+  const [drawStatus, setDrawStatus] = useState(true);
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const { offsetX, offsetY } = e.nativeEvent;
+    if (drawStatus) {
+      if (first.x !== 0 && first.y !== 0) {
+        if (first.x < offsetX) {
+          setWidth(offsetX - first.x);
+        }
+        if (first.y < offsetY) {
+          setHeight(offsetY - first.y);
+        }
+      }
+    }
+  };
+
+  const handleMouseUp = () => {
+    setDrawStatus(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleDraw}>네모</button>
+      <span>{draw ? "그릴수있다" : "그릴수없다"}</span>
+      <Canvas
+        onMouseDown={hanldeMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+      >
+        {first.x > 0 && first.y > 0 && (
+          <Box x={first.x} y={first.y} width={width} height={height} />
+        )}
+      </Canvas>
     </div>
   );
 }
